8a89a24b0af925d65f9f72a206883ba07c3ee775
Naresh Sharma <nsharma@okkular.io>
2023-04-20 12:28:13 +0530

    episode 3 laying the foundation.

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// JSX: babel transpiles it to React.createElement("h1",{})
+// const jsxHeader = (<h1>
+//     Hello from JSX heading
+//     </h1>
+//     )
+// console.log(jsxHeader)
+
+// REACT FUNCTIONAL COMPONENT(). 
+let dynamicTitle= "Namaste javascript"
+const Title= () =>{
+    return <h1>{dynamicTitle}</h1>
+}
+const TitleDesc= () =>{
+    return <p>This is the demo title text.</p>
+}
+const Card= () =>{
+
+    return(
+        <div id="card">
+            {/* component composition */}
+            <Title></Title>
+            <TitleDesc/>
+
+        </div>
+    )
+}
+
+let cc = Card()
+console.log("CARD:::",cc)
+
+const TitleComponent = () => <h1>This is a functional Title component</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// const heading = React.createElement('h1',{id:"heading"},"Hello world from react!"); 
-// root.render(heading)
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
 
+// root.render(jsxHeader)
+root.render(<Card />)
+root1.render(<Card />)
 
-// CREATING A HIEARCHY DOM
-
-const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, [React.createElement('h1', {}, "Hello Naresh Sharma")]));
-root.render(parent);
